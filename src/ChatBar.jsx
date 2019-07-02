8d0d87413e10c95104adb775f240ad8fc64d19f0
Nick <nbjenvey@gmail.com>
2019-07-02 01:50:39 +0000

    Passed currentUser prop to ChatBar

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: props.currentUser.name };
+  }
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" value={this.state.user} placeholder="Your Name (Optional)" />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
