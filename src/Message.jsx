7da7f5c83b750f5261b9684a1b8591cbb47e9bc7
Nick <nbjenvey@gmail.com>
2019-07-02 01:57:50 +0000

    Started working on getting the messages passed in

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 class Message extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="message">
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 import Message from "./Message.jsx"
 
 class MessageList extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const messageItems = props.messages.map((username, content) => {
+      
+    });
     return (
       <main className="messages">
         <Message />
